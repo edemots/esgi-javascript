0abc354528e0785cf1c2850612879e11355eb331
Eliot Demots <eliot@demots.fr>
2019-11-27 12:35:57 +0100

    type_check_v1

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,5 +1,12 @@
 "use strict";
 
 function type_check_v1(entry, type) {
-  return
+  switch (type) {
+    case "null":
+      return entry === null;
+    case "array":
+      return entry.constructor === Array
+    default:
+      return typeof entry === type.toLowerCase();
+  }
 }
