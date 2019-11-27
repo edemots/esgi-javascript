be321628ed542e506fc6b05c747cf11be66ba9d5
Eliot Demots <eliot@demots.fr>
2019-11-27 14:04:51 +0100

    fixes

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -10,6 +10,6 @@ function type_check_v2(input, conf = {type: "", value: "", enum: []}) {
   if (Object.prototype.hasOwnProperty.call(conf, "type") && !type_check_v1(input, conf.type))
     return false;
   if (Object.prototype.hasOwnProperty.call(conf, "value") && input !== conf.value)
-    return false;
-  return !(Object.prototype.hasOwnProperty.call(conf, "enum") && !conf.enum.contains(input));
+    return false;s
+  return !(Object.prototype.hasOwnProperty.call(conf, "enum") && !conf.enum.includes(input));
 }
