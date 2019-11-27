d34fd68ab2487cae9090e12d4acfe6cab40d749d
Eliot Demots <eliot@demots.fr>
2019-11-27 13:08:08 +0100

    fixes

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,7 +1,15 @@
 'use strict'
 
-function type_check_v1 (input, type) {
+function type_check_v1(input, type) {
   if (input === null) return type === "null";
   if (Array.isArray(input)) return type === "array";
   return typeof input === type;
+}
+
+function type_check_v2(input, conf = {type: "", value: "", enum: []}) {
+  if (Object.prototype.hasOwnProperty.call(conf, "type") && !type_check_v1(input, conf.type))
+    return false;
+  if (Object.prototype.hasOwnProperty.call(conf, "value") && input !== conf.value)
+    return false;
+  return !(Object.prototype.hasOwnProperty.call(conf, "enum") && !conf.enum.contains(input));
 }
